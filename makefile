c1121a048e244b4fb046b7567c96f3adfc8d0a93
JohnCoene <jcoenep@gmail.com>
2021-03-07 22:38:38 +0100

    add color to tailwind

--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 build:
 				npm install
+				npm run build-css	
 				npm run build	
 				go build
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#262b36"
+      }
+    },
   },
   variants: {
     extend: {},
