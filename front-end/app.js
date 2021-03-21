7c52fc50feebf0137d571dc3c8abd6426700b7f6
JohnCoene <jcoenep@gmail.com>
2021-03-21 21:54:53 +0100

    add warning

--- front-end/app.js
+++ front-end/app.js
@@ -1,1 +1,9 @@
 console.log("Hello, world");
+
+document.addEventListener("DOMContentLoaded",function(){
+  let locationsInput = document.getElementById("locationsStream");
+  let locationsWarning = document.getElementById("locationsWarning");
+  locationsInput.addEventListener("input", (event) => {
+    locationsWarning.innerHTML = "This dramatically reduces the number of tweets streamed, see the documentation."
+  });
+});
