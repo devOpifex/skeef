ba535b804a0a832de9bfae3da940ca2bf12edd26
JohnCoene <jcoenep@gmail.com>
2021-03-21 22:02:57 +0100

    add warnings

--- front-end/app.js
+++ front-end/app.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded",function(){
   let locationsInput = document.getElementById("locationsStream");
   let locationsWarning = document.getElementById("locationsWarning");
   locationsInput.addEventListener("input", (event) => {
+    if(event.data == null){
+      locationsWarning.innerHTML = "";
+      return ;
+    }
     locationsWarning.innerHTML = "This dramatically reduces the number of tweets streamed, see the documentation."
   });
 });
