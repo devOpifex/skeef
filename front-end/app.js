6ef6a58389e76fe40cbfca95c55d99962d5ae848
JohnCoene <jcoenep@gmail.com>
2021-03-24 23:09:44 +0100

    prototype viz

--- front-end/app.js
+++ front-end/app.js
@@ -1,3 +1,6 @@
+var createGraph = require('ngraph.graph');
+var pixel = require('ngraph.pixel');
+
 document.addEventListener("DOMContentLoaded",function(){
 
   // warning on admin inputs
@@ -14,6 +17,53 @@ document.addEventListener("DOMContentLoaded",function(){
     });
   }
 
+  var g = createGraph();
+  g.addNode(1, {type: 'hidden'});
+  g.addNode(2, {type: 'hidden'});
+
+  if(document.getElementById("graph") != null){
+    var renderer = pixel(g, {
+      is3d: true,
+      container: document.getElementById("graph"),
+      physics: {
+        springLength : 80,
+        springCoeff : 0.0002,
+        gravity: -1.2,
+        theta : 0.8,
+        dragCoeff : 0.02
+      },
+      node: function (n) {
+        if(n.data.type == 'user'){
+          return {
+            color: 0xFFC6FF,
+            size: n.data.count * 10
+          }
+        } else if(n.data.type == 'hashtag'){
+          return {
+            color: 0x9BF6FF,
+            size: n.data.count * 10
+          }
+        } else if (n.data.type == 'hidden') {
+          return {
+            color: 0x000000,
+            size: 0
+          }
+        } else {
+          return {
+            color: 0xFFFFFF,
+            size: 15
+          }
+        }
+      },
+      link: function(l){
+        return {
+          fromColor: 0xCCCCCC,
+          toColor: 0xCCCCCC
+        }
+      }
+    });
+  }
+
   // websocket
   window.socket.onopen = () => {
     socket.send("Client Connected")
@@ -26,10 +76,35 @@ document.addEventListener("DOMContentLoaded",function(){
   window.socket.onerror = (error) => {
     console.log(error);
   }
-  
+
   window.socket.onmessage = (data) => {
-    console.log("got data");
     let parsed = JSON.parse(data.data);
     console.log(parsed);
+
+    g.beginUpdate();
+    if(parsed.graph.nodes){
+      for(let i = 0; i < parsed.graph.nodes.length; i++)  {
+
+        if(g.hasNode(parsed.graph.nodes[i].name)){
+          continue;
+        }
+
+        g.addNode(
+          parsed.graph.nodes[i].name,
+          {type: parsed.graph.nodes[i].type, count: parsed.graph.nodes[i].count}
+        );
+      }
+    }
+
+    if(parsed.graph.edges){
+      for(let i = 0; i < parsed.graph.edges.length; i++){
+        if(g.hasLink(parsed.graph.edges[i].source, parsed.graph.edges[i].target)){
+          continue; 
+        }
+
+        g.addLink(parsed.graph.edges[i].source, parsed.graph.edges[i].target);
+      }
+    }
+    g.endUpdate();
   }
 });
