b715ad0c68766f8c6911d7cdd4551486dc1b9b39
JohnCoene <jcoenep@gmail.com>
2021-03-29 22:32:37 +0200

    add ntweets

--- front-end/app.js
+++ front-end/app.js
@@ -1,6 +1,3 @@
-import ForceGraph3D from '3d-force-graph';
-import SpriteText from 'three-spritetext';
-
 document.addEventListener("DOMContentLoaded",function(){
 
   // warning on admin inputs
@@ -17,33 +14,6 @@ document.addEventListener("DOMContentLoaded",function(){
     });
   }
 
-  let graphElem = document.getElementById("graph");
-  let Graph;
-  if(graphElem != null){
-    const initData = {
-      nodes: [],
-      links: []
-    };
-
-    Graph = ForceGraph3D()(graphElem)
-      .onNodeHover(node => graphElem.style.cursor = node ? 'pointer' : null)
-      .graphData(initData)
-      .enableNodeDrag(false)
-      .backgroundColor('#262b36')
-      .nodeAutoColorBy('type')
-      .nodeVal('value')
-      .nodeThreeObject(node => {
-        const sprite = new SpriteText(node.id);
-        sprite.material.depthWrite = false; // make sprite background transparent
-        sprite.color = node.color;
-        sprite.textHeight = node.value;
-        return sprite;
-      })
-      .linkDirectionalParticles("value")
-      .linkDirectionalParticleSpeed(d => d.value * 0.001);
-
-  }
-
   // websocket
   window.socket.onopen = () => {
     socket.send("Client Connected")
@@ -57,9 +27,7 @@ document.addEventListener("DOMContentLoaded",function(){
     console.log(error);
   }
 
-  let firstrun = true;
   let ntweets = document.getElementById("ntweets");
-
   window.socket.onmessage = (data) => {
     let parsed = JSON.parse(data.data);
     console.log(parsed);
@@ -70,66 +38,5 @@ document.addEventListener("DOMContentLoaded",function(){
 
     ntweets.innerText = parsed.tweetsCount;
 
-    let { nodes, links } = Graph.graphData();
-
-    if (firstrun == true){
-      Graph.graphData({
-        nodes: [...nodes, ...parsed.graph.nodes],
-        links: [...links, ...parsed.graph.edges]
-      });
-      firstrun = false;
-      return ;
-    }
-
-    // only nodes to add
-    let nodesAdditions = parsed.graph.nodes.filter(function(n){
-      return n.action == "add"
-    })
-
-    if(parsed.graph.nodes != null){
-      let nodeUpdates = parsed.graph.nodes.filter(function(n){
-        return n.action == "update"
-      });
-  
-      for(let i = 0; i < nodeUpdates.length; i++){
-        for(let j = 0; j < nodes.length; j++){
-          if(nodes[j].id == nodeUpdates[i].name){
-            nodes[j].value = nodeUpdates[i].value
-          }
-        }
-      }
-    }
-
-    let edgesAdditions = parsed.graph.edges.filter(function(e){
-      return e.action == "add"
-    });
-
-    if(parsed.graph.nodes != null){
-      let edgeUpdates = parsed.graph.nodes.filter(function(e){
-        return e.action == "update"
-      });
-  
-      for(let i = 0; i < edgeUpdates.length; i++){
-        for(let j = 0; j < links.length; j++){
-          if(links[j].id == edgeUpdates[i].name){
-            links[j].value = edgeUpdates[i].value
-          }
-        }
-      }
-    }
-
-    // remove
-    if(parsed.removeNodes.length > 0){
-      for(let node of parsed.removeNodes){
-        links = links.filter(l => l.source !== node && l.target !== node);
-        nodes = nodes.filter(n => n.id != node);
-      }
-    }
-
-    Graph.graphData({
-      nodes: [...nodes, ...nodesAdditions],
-      links: [...links, ...edgesAdditions]
-    });
-  
   }
 });
